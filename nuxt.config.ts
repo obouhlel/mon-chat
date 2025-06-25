9b881756eeff5e73472ef9985c1781e4519af829
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-06-25 09:39:31 +0200

    Install supabase and configure and test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+
   devtools: { enabled: true },
+
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_ANON_KEY,
+    redirect: false,
+  },
+
   css: ['~/assets/css/main.css'],
+
   modules: [
     "@nuxt/ui",
     "@nuxt/image",
     "@nuxt/icon",
     "@nuxt/fonts",
-    "@nuxt/eslint"
+    "@nuxt/eslint",
+    "@nuxtjs/supabase"
   ]
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+import { useSupabaseClient } from '#imports';
+import type { Database } from '~/types/supabase.type';
+
+const supabase = useSupabaseClient<Database>();
+
+const { data, error } = await supabase.from('message').select('*').limit(1);
+</script>
+
 <template>
   <div class="w-full min-h-[calc(100vh-56px-32px)] flex items-center justify-center">
-    <h1 class="text-2xl font-bold">Hello, world !</h1>
+    <h1 v-if="data" class="text-2xl font-bold">{{ data[0].content }}, world !</h1>
+    <h1 v-else class="text-red-500 font-bold">{{ error }}</h1>
   </div>
 </template>
