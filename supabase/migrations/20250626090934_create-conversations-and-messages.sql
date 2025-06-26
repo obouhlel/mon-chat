2f62aad38fdddec4dd5d4d1bf5a909beb5a90cb2
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-06-26 12:15:41 +0200

    First migration for conversation

---
+++ supabase/migrations/20250626090934_create-conversations-and-messages.sql
@@ -0,0 +1,14 @@
+create table conversations (
+  id uuid primary key default gen_random_uuid(),
+  user1_id uuid not null references auth.users(id) on delete cascade,
+  user2_id uuid not null references auth.users(id) on delete cascade,
+  created_at timestamp with time zone default now()
+);
+
+create table messages (
+  id uuid primary key default gen_random_uuid(),
+  conversation_id uuid not null references conversations(id) on delete cascade,
+  sender_id uuid not null references auth.users(id) on delete cascade,
+  content text not null,
+  created_at timestamp with time zone default now()
+);
