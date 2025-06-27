83b7ff1fdd52e6612bc1f74a9f4e3b4350242c01
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-06-27 06:14:17 +0200

    Update the .env, add SUPABASE_SERVICE_ROLE_KEY to get user with admin command

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
 
   supabase: {
     url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_ANON_KEY,
+    key: process.env.SUPABASE_SERVICE_ROLE_KEY,
     redirect: false,
   },
 
