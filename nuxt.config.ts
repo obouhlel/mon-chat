7006e5841495ee693a15631458f94a6bcd88911c
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-06-29 09:53:38 +0200

    Update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,15 @@ export default defineNuxtConfig({
 
   devtools: { enabled: true },
 
+  runtimeConfig: {
+    public: {
+      supabase: {
+        url: process.env.SUPABASE_URL,
+        key: process.env.SUPABASE_ANON_KEY,
+      }
+    }
+  },
+
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_ANON_KEY,
